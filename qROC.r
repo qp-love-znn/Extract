c06cee80cf3406c486f013fa4391140b611cdfa8
qp-love-znn <45379278+qp-love-znn@users.noreply.github.com>
2018-12-15 16:42:40 +0800

    Update qROC.r

--- qROC.r
+++ qROC.r
@@ -27,3 +27,26 @@ for (i in 1:ncol(trdata)) {
 tmp<-cbind(mat,auc_value)
 result<-cbind(meta_names,tmp)
 write.table(result,file = paste(args[3], '.auc.xls', sep = ''),quote = FALSE, sep = "\t",row = F,)
+
+##cor
+library("corrplot")
+mat<- read.table(args[1], header = T,sep="\t",check.name=0,comment.char = "")
+aa<-as.matrix(mat[,2:ncol(mat)])
+row.names(aa)<-mat[,1]
+bb=colorRampPalette(c("blue", "white", "red"))
+corrplot(aa,method = "ellipse",tl.cex = 0.4,col = bb(100),tl.col = "black")
+
+##tree
+library(ape)
+g<-read.table(args[1], header = T,sep="\t",check.name=0,comment.char = "")
+color<-hsv(runif(length(unique(g$V2)),0.65,0.95),1,1,0.7)
+names(color)<-unique(g$V2)
+g2<-g
+g2$V2<-color[g$V2]
+color_new<-g2$V2
+names(color_new)<-g$V1
+color_new2<-color_new[tree$tip.label]
+tree <- read.tree(args[2])
+plot(tree,type = "unrooted",cex=0.2,edge.lty=1)
+plot(tree,type = "unrooted",tip.color = color_new2)
+?plot.phylo
