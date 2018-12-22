d5915fe22754d5889fc6445a42e8e1b97f1d160f
qp-love-znn <45379278+qp-love-znn@users.noreply.github.com>
2018-12-22 15:51:47 +0800

    Update ggplot.r

--- ggplot.r
+++ ggplot.r
@@ -2,5 +2,5 @@
 library(ggrepel)
 ggplot(mtcars)+ 
   geom_point(aes(wt, mpg), color="grey", size=5)+
-  geom_label_repel(aes(wt, mpg, fill=factor(cyl),label=rownames(mtcars)),arrow = arrow(length=unit(0.01, "npc")))
+  geom_label_repel(aes(wt, mpg, fill=factor(cyl),label=rownames(mtcars)，angle=ifelse(mtcars$cyl==4, 90, 0)),arrow = arrow(length=unit(0.01, "npc")))
   
